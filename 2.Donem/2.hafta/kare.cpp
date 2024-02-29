44951cff8935040036a690065b24c9f6ef865fec
HikmeT <hekmt111@gmail.com>
2024-02-29 14:22:52 +0300

    kare

---
+++ 2.Donem/2.hafta/kare.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+#define log(x) std::cout << x << std::endl 
+
+int kare(int x){
+    return x*x;
+}
+
+int main(){
+
+    log("sss");
+
+    return 0;
+
+}
---
+++ 2.Donem/2.hafta/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+#define log(x) std::cout << x << std::endl 
+
+void weclome();
+int main(){
+
+    /* log("sss"); */
+    weclome();
+    return 0;
+
+}
+void weclome(){
+    cout << "WELCOME"<<endl;
+}
