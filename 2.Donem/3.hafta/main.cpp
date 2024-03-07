ea51da01c66e0a035cc0ec8665e8cd57e736cdce
HikmeT <hekmt111@gmail.com>
2024-03-07 14:05:40 +0300

    Pointers - pointer1

--- 2.Donem/3.hafta/main.cpp
+++ 2.Donem/3.hafta/main.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 int main(){
 
+    int* ptr;
+    int a = 5;
+
+    ptr = &a;
+
+    *ptr = 6;
+    cout << a << endl;
+    cout << ptr << endl;
+    cout << *ptr ;
+
 
     return 0;
 }
---
+++ 2.Donem/3.hafta/pointer1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a = 2;
+    cout << a <<endl;
+    cout << &a <<endl;
+    cout << sizeof(a) <<endl;
+
+
+    return 0;
+}
