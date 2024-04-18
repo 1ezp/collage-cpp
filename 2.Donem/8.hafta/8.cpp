320e5abbe8146a91adc854e4dd1472241f9c0ec1
HikmeT <hekmt111@gmail.com>
2024-04-18 14:50:07 +0300

    getters and setters

---
+++ 2.Donem/8.hafta/8.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+class Date{
+
+    int m_day;
+    int m_month;
+    int m_year;
+
+    public:
+    int getMonth(){return m_month;}
+    void setMonth(int month){m_month = month;};
+    
+    int getDay(){return m_day;}
+    void setMonth(int day){m_day = day;};
+    
+    int getYear(){return m_year;}
+    void setYear(int year){m_year = year;};
+
+}
+
+int main(){
+
+
+    return 0;
+}
