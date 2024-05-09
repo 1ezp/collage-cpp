09b714b3f2f90092fc200da30ffbb3c5f6418fe2
HikmeT <hekmt111@gmail.com>
2024-05-09 19:56:24 +0300

    friend func and class

---
+++ 2.Donem/9.hafta/arkadas1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+class Nesne{
+    private:
+        int m_deger {0};
+
+    public:
+        void ekle(int deger){
+            m_deger += deger;
+        }
+        friend void sil(Nesne nesne);
+
+};
+
+
+void sil(Nesne nesne){
+    // delete the varaible m_deger be set to 0 lol
+    nesne.m_deger = 0;
+    cout << nesne.m_deger << endl;
+}
+
+
+int main(){
+    Nesne nesne1;
+    nesne1.ekle(5);
+    nesne1.ekle(6);
+    sil(nesne1);
+
+    return 0;
+}
+
